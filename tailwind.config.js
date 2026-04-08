9b4671f7e775c6fe7260c25e940423142953a82e
Matheus Silva <matheussilva23132016@gmail.com>
2026-04-08 11:30:41 -0300

    volta15

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 module.exports = {
-  theme: {
-    extend: {
-      animation: {
-        'pulse': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-      },
-      colors: {
-        blue: {
-          400: '#60a5fa',
-          500: '#3b82f6',
-          600: '#2563eb',
-        },
-      },
-    },
-  },
-};
+ content: [
+ "./app/**/*.{js,ts,jsx,tsx,mdx}",
+ "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+ "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ "./src/**/*.{js,ts,jsx,tsx,mdx}",
+ ],
+ theme: {
+ extend: {},
+ },
+ plugins: [],
+}
